f7369fb9f09af294a392037b6d6c13270fe6339a
T-InstaApp <144309913+T-InstaApp@users.noreply.github.com>
2024-03-08 17:45:31 +0530

    Update StaticValue.kt

--- app/src/main/java/com/instaapp/clothingtemplate1/utils/StaticValue.kt
+++ app/src/main/java/com/instaapp/clothingtemplate1/utils/StaticValue.kt
@@ -6,7 +6,7 @@ object StaticValue {
     private const val PRODUCTION_BASE_URL = "https://restaurant60-be-prod-xtpocjmkpa-uw.a.run.app/"
     private const val DEVELOPMENT_BASE_URL = "https://restaurant60-be-dev-xtpocjmkpa-uw.a.run.app/"
     const val BASE_URL = DEVELOPMENT_BASE_URL
-    const val REST_ID = ""239""
+    const val REST_ID = "239"
     "
     val SALUTATION_DATA = arrayOf("Mr.", "Mrs.", "Miss.", "Ms.")
     const val DOT = "•"
